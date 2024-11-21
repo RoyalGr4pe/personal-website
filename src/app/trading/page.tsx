330a94ebe220bd65b64af316479e9152427e5c3e
RoyalGr4pe <njames.programming@gmail.com>
2024-11-21 10:42:24 +0000

    profit calculator

--- src/app/trading/page.tsx
+++ src/app/trading/page.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import CalculateMarketCap from './CalculateMarketCap'
-
+import MarketCapToBreak from './MarketCapToBreak'
+import MarketCapProfits from './MarketCapProfits'
 
 
 const page = () => {
 	return (
-		<CalculateMarketCap />
+		<div className='h-screen flex lg:flex-row flex-col justify-center items-center p-4 gap-4'>
+			<MarketCapToBreak />
+			<MarketCapProfits />
+		</div>
 	)
 }
 
